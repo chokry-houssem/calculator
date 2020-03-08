548281b486fcf7d540867cedd817b4b5d884d963
houssemchokri <61630874+houssemchokri@users.noreply.github.com>
2020-03-08 11:10:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+function input1(){
+	calculator.t.value += '1';
+}
+function input2(){
+	calculator.t.value += '2';
+}
+function inputP(){
+calculator.t.value += '+';
+}
+function inputU(){
+	calculator.t.value = eval(calculator.t.value);
+}
+function input0(){
+	calculator.t.value += '0';
+}
+function input3(){
+	calculator.t.value += '3' ; 
+}
+function input4(){
+	calculator.t.value += '4' ; 
+}
+function input5(){
+	calculator.t.value += '5' ; 
+}
+function input6(){
+	calculator.t.value += '6' ; 
+}
+function input7(){
+	calculator.t.value += '7' ; 
+}
+function input8(){
+	calculator.t.value += '8' ; 
+}
+function input9(){
+	calculator.t.value += '9' ; 
+}
+function inputF(){
+	calculator.t.value+= '*' ;
+}
+function inputV(){
+	calculator.t.value += '/' ; 
+}
+function inputC(){
+	calculator.t.value = ' ' ; 
+}
+function inputM(){
+	calculator.t.value += '-' ; 
+}
